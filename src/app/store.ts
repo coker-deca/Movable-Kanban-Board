264c06c0d579f5d9c65014738d6d4ff56d14427b
tosincoker26 <tosincoker26@gmail.com>
2022-03-12 19:12:46 +0100

    Setup redux toolkit and persist

---
+++ src/app/store.ts
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { boardApi } from '../services';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, boardApi.reducer)
+
+export const createStore = () =>
+  configureStore({
+    reducer: {
+      [boardApi.reducerPath]: persistedReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(boardApi.middleware),
+});
+
+export const store = createStore();
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export type RootState = ReturnType<typeof store.getState>;
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+setupListeners(store.dispatch);
--- src/services/index.ts
+++ src/services/index.ts
@@ -3,36 +3,43 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Board, Comments, Task } from '../constants/types';
 
 export const boardApi = createApi({
-  reducerPath: "boardApi",
+  reducerPath: 'boardApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://company-lookup.herokuapp.com/api/v1',
+    baseUrl: 'https://my-json-server.typicode.com/coker-deca/fake-server',
   }),
   tagTypes: ['Boards', 'Comments', 'Tasks'],
   endpoints: (builder) => ({
     getBoards: builder.query<Board[], void>({
-    query: () => ({url:"/boards", method: "GET"})
-  }),
+      query: () => ({ url: '/boards', method: 'GET' }),
+    }),
     addBoards: builder.mutation<Board[], Board[]>({
-    query: () => ({url:"/boards", method: "POST"}),
-      invalidatesTags: [{ type: 'Boards', id: 'LIST' }]
-  }),
+      query: () => ({ url: '/boards', method: 'POST' }),
+      invalidatesTags: [{ type: 'Boards', id: 'LIST' }],
+    }),
     getTasks: builder.query<Task[], void>({
-      query: () => ({ url: "/tasks", method: 'GET' }),
-      providesTags: [{ type: 'Tasks', id: 'LIST' }]
-  }),
+      query: () => ({ url: '/tasks', method: 'GET' }),
+      providesTags: [{ type: 'Tasks', id: 'LIST' }],
+    }),
     addTasks: builder.mutation<Task[], Task[]>({
-      query: () => ({ url: "/tasks", method: 'POST' }),
-      invalidatesTags: [{ type: 'Tasks', id: 'LIST' }]
-  }),
+      query: () => ({ url: '/tasks', method: 'POST' }),
+      invalidatesTags: [{ type: 'Tasks', id: 'LIST' }],
+    }),
     getComments: builder.query<Comments[], void>({
-      query: () => ({ url: "/comments", method: 'GET' }),
-      providesTags: [{ type: 'Comments', id: 'LIST' }]
+      query: () => ({ url: '/comments', method: 'GET' }),
+      providesTags: [{ type: 'Comments', id: 'LIST' }],
     }),
     addComments: builder.mutation<Comments[], Comments[]>({
-      query: () => ({ url: "/comments", method: 'POST' }),
-      invalidatesTags: [{ type: 'Comments', id: 'LIST' }]
+      query: () => ({ url: '/comments', method: 'POST' }),
+      invalidatesTags: [{ type: 'Comments', id: 'LIST' }],
     }),
   }),
 });
 
-export const { useGetBoardsQuery, useGetCommentsQuery, useGetTasksQuery, useAddBoardsMutation, useAddCommentsMutation, useAddTasksMutation} = boardApi;
+export const {
+  useGetBoardsQuery,
+  useGetCommentsQuery,
+  useGetTasksQuery,
+  useAddBoardsMutation,
+  useAddCommentsMutation,
+  useAddTasksMutation,
+} = boardApi;
